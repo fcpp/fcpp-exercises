23d0c6c1dc29740cee897d1faeb5eeebc1f70f3e
Giorgio Audrito <giorgio.audrito@gmail.com>
2021-08-24 16:17:59 +0200

    initial version of the exercises

---
+++ README.md
@@ -0,0 +1,82 @@
+# FCPP Starting Exercises
+
+Quick-start aggregate computing exercises. The exercises are described in the `run/exercises.cpp` file. All commands below are assumed to be issued from the cloned git repository folder. For any issues, please contact [Giorgio Audrito](mailto:giorgio.audrito@unito.it).
+
+
+## References
+
+- FCPP main website: [https://fcpp.github.io](https://fcpp.github.io).
+- FCPP documentation: [http://fcpp-doc.surge.sh](http://fcpp-doc.surge.sh).
+- FCPP sources: [https://github.com/fcpp/fcpp](https://github.com/fcpp/fcpp).
+
+
+## Setup
+
+The next sections contain the FCPP setup instructions for the various supported OSs.
+
+### Windows
+
+Pre-requisites:
+- [Git Bash](https://gitforwindows.org) (for issuing unix-style commands)
+- [MinGW-w64 builds 8.1.0](http://mingw-w64.org/doku.php/download/mingw-builds)
+- [CMake 3.9](https://cmake.org) (or higher)
+- [Asymptote](http://asymptote.sourceforge.io) (for building the plots)
+
+During CMake installation, make sure you select to add `cmake` to the `PATH` (at least for the current user).
+During MinGW installation, make sure you select "posix" threads (should be the default) and not "win32" threads. After installing MinGW, you need to add its path to the environment variable `PATH`. The default path should be:
+```
+C:\Program Files (x86)\mingw-w64\i686-8.1.0-posix-dwarf-rt_v6-rev0\mingw32\bin
+```
+but the actual path may vary depending on your installation.
+
+### Linux
+
+Pre-requisites:
+- Xorg-dev package (X11)
+- G++ 9 (or higher)
+- CMake 3.9 (or higher)
+- Asymptote (for building the plots)
+
+To install these packages in Ubuntu, type the following command:
+```
+sudo apt-get install xorg-dev g++ cmake asymptote
+```
+
+### MacOS
+
+Pre-requisites:
+- Xcode Command Line Tools
+- CMake 3.9 (or higher)
+- [Asymptote](http://asymptote.sourceforge.io) (for building the plots)
+
+To install them, assuming you have the [brew](https://brew.sh) package manager, type the following commands:
+```
+xcode-select --install
+brew install cmake asymptote
+```
+
+
+## Execution
+
+In order to execute the exercises file, type the following command in a terminal:
+```
+> ./make.sh gui run -O exercises
+```
+Running the above command, you should see output about building the executables then the graphical simulation should pop up.
+
+### Graphical User Interface
+
+Executing a graphical simulation will open a window displaying the simulation scenario, initially still: you can start running the simulation by pressing `P` (current simulated time is displayed in the bottom-left corner). While the simulation is running, network statistics will be periodically printed in the console. You can interact with the simulation through the following keys:
+- `Esc` to end the simulation
+- `P` to stop/resume
+- `O`/`I` to speed-up/slow-down simulated time
+- `L` to show/hide connection links between nodes
+- `G` to show/hide the grid on the reference plane and node pins
+- `M` enables/disables the marker for selecting nodes
+- `left-click` on a selected node to open a window with node details
+- `C` resets the camera to the starting position
+- `Q`,`W`,`E`,`A`,`S`,`D` to move the simulation area along orthogonal axes
+- `right-click`+`mouse drag` to rotate the camera
+- `mouse scroll` for zooming in and out
+- `left-shift` added to the camera commands above for precision control
+- any other key will show/hide a legenda displaying this list
---
+++ run/exercises.cpp
@@ -0,0 +1,183 @@
+// Copyright © 2021 Giorgio Audrito. All Rights Reserved.
+
+/**
+ * @file exercises.cpp
+ * @brief Quick-start aggregate computing exercises.
+ */
+
+#include "lib/fcpp.hpp"
+
+/**
+ * @brief Namespace containing all the objects in the FCPP library.
+ */
+namespace fcpp {
+
+//! @brief Namespace containing the libraries of coordination routines.
+namespace coordination {
+
+//! @brief Tags used in the node storage.
+namespace tags {
+    //! @brief Color of the current node.
+    struct node_color {};
+    //! @brief Size of the current node.
+    struct node_size {};
+    //! @brief Shape of the current node.
+    struct node_shape {};
+    // ... add more as needed, here and in the tuple_store<...> option below
+}
+
+/**
+ * EXERCISES:
+ *
+ * Expand the MAIN function below to compute the following:
+ *
+ * 1)    The number of neighbour devices.
+ *
+ * 2)    The maximum number of neighbour devices ever witnessed by the current device.
+ *
+ * 3)    The maximum number of neighbour devices ever witnessed by any device in the network.
+ *
+ * 4)    Move towards the neighbour with the lowest number of neighbours.
+ *
+ * Every exercise above is designed to help solving the following one.
+ *
+ * In order to check whether what you computed is correct, you may display the computed
+ * quantities as node qualities through tags `node_color`, `node_size` and `node_shape`.
+ * You can also save your computed quantities in additional specific node attributes:
+ * towards this end, you should both add a tag in namespace tags above, then list it
+ * (together with the corresponding data type) in the `tuple_store` option below.
+ *
+ *
+ * BONUS EXERCISES:
+ *
+ * 5)    Move away from the neighbour with the highest number of neighbours.
+ *
+ * 6)    Move as if the device was attracted by the neighbour with the lowest number of neighbours,
+ *       and repulsed by the neighbour with the highest number of neighbours.
+ *
+ * 7)    Move as if the device was repulsed by every neighbour, and by the four walls of the
+ *       rectangular box between points [0,0] and [500,500].
+ *
+ * HINTS:
+ *
+ * -    In the first few exercises, start by reasoning on when/where to use `nbr` (collecting from
+ *      neighbours) and `old` (collecting from the past).
+ *
+ * -    In order to move a device, you need to set a velocity vector through something like
+ *      `node.velocity() = make_vec(0,0)`.
+ *
+ * -    Coordinates are available through `node.position()`. Coordinates can be composed as physical
+ *      vectors: `[1,3] + [2,-1] == [3,2]`, `[2,4] * 0.5 == [1,2]`.
+ *
+ * -    In the last two exercises, you can model attraction/repulsion using the classical inverse square law.
+ *      More precisely, if `v` is the vector between two objects, the resulting force is `v / |v|^3` where
+ *      `|v| = sqrt(v_x^2 + v_y^2)`. In FCPP, `norm(v)` is available for computing `|v|`.
+ */
+
+
+//! @brief Main function.
+MAIN() {
+    // import tag names in the local scope.
+    using namespace tags;
+
+    // sample code below (substitute with the solution to the exercises)...
+
+    // usage of aggregate constructs
+    field<double> f = nbr(CALL, 4.2); // nbr with single value
+    int x = old(CALL, 0, [&](int a){  // old with initial value and update function
+        return a+1;
+    });
+    int y = nbr(CALL, 0, [&](field<int> a){ // nbr with initial value and update function
+        return min_hood(CALL, a);
+    });
+
+    // usage of node physics
+    node.velocity() = -node.position()/100;
+
+    // usage of node storage
+    node.storage(node_size{}) = 10;
+    node.storage(node_color{}) = color(GREEN);
+    node.storage(node_shape{}) = shape::sphere;
+}
+//! @brief Export types used by the main function (update it when expanding the program).
+FUN_EXPORT main_t = common::export_list<double, int>;
+
+} // namespace coordination
+
+
+//! @brief Namespace for component options.
+namespace option {
+
+//! @brief Import tags to be used for component options.
+using namespace component::tags;
+//! @brief Import tags used by aggregate functions.
+using namespace coordination::tags;
+
+//! @brief Number of people in the area.
+constexpr int node_num = 100;
+//! @brief Dimensionality of the space.
+constexpr size_t dim = 2;
+
+//! @brief Description of the round schedule.
+using round_s = sequence::periodic<
+    distribution::interval_n<times_t, 0, 1>,    // uniform time in the [0,1] interval for start
+    distribution::weibull_n<times_t, 10, 1, 10> // weibull-distributed time for interval (10/10=1 mean, 1/10=0.1 deviation)
+>;
+//! @brief The sequence of network snapshots (one every simulated second).
+using log_s = sequence::periodic_n<1, 0, 1>;
+//! @brief The sequence of node generation events (node_num devices all generated at time 0).
+using spawn_s = sequence::multiple_n<node_num, 0>;
+//! @brief The distribution of initial node positions (random in a 500x500 square).
+using rectangle_d = distribution::rect_n<1, 0, 0, 500, 500>;
+//! @brief The contents of the node storage as tags and associated types.
+using store_t = tuple_store<
+    node_color,                 color,
+    node_size,                  double,
+    node_shape,                 shape
+>;
+//! @brief The tags and corresponding aggregators to be logged (change as needed).
+using aggregator_t = aggregators<
+    node_size,                  aggregator::mean<double>
+>;
+
+//! @brief The general simulation options.
+DECLARE_OPTIONS(list,
+    parallel<true>,      // multithreading enabled on node rounds
+    synchronised<false>, // optimise for asynchronous networks
+    program<coordination::main>,   // program to be run (refers to MAIN above)
+    exports<coordination::main_t>, // export type list (types used in messages)
+    retain<metric::retain<2,1>>,   // messages are kept for 2 seconds before expiring
+    round_schedule<round_s>, // the sequence generator for round events on nodes
+    log_schedule<log_s>,     // the sequence generator for log events on the network
+    spawn_schedule<spawn_s>, // the sequence generator of node creation events on the network
+    store_t,       // the contents of the node storage
+    aggregator_t,  // the tags and corresponding aggregators to be logged
+    init<
+        x,      rectangle_d // initialise position randomly in a rectangle for new nodes
+    >,
+    dimension<dim>, // dimensionality of the space
+    connector<connect::fixed<100, 1, dim>>, // connection allowed within a fixed comm range
+    shape_tag<node_shape>, // the shape of a node is read from this tag in the store
+    size_tag<node_size>,   // the size  of a node is read from this tag in the store
+    color_tag<node_color>  // the color of a node is read from this tag in the store
+);
+
+} // namespace option
+
+} // namespace fcpp
+
+
+//! @brief The main function.
+int main() {
+    using namespace fcpp;
+
+    //! @brief The network object type (interactive simulator with given options).
+    using net_t = component::interactive_simulator<option::list>::net;
+    //! @brief The initialisation values (simulation name).
+    auto init_v = common::make_tagged_tuple<option::name>("Exercises");
+    //! @brief Construct the network object.
+    net_t network{init_v};
+    //! @brief Run the simulation until exit.
+    network.run();
+    return 0;
+}
